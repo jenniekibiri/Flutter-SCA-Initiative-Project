41abedd37b06c63d29fc94994ed801acc71651c1
jenniekibiri <josphatje@gmail.com>
2021-01-13 14:02:02 +0300

    add logic part

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double output = 0, num1 = 0, num2 = 0;
+  TextEditingController t1 = TextEditingController(text: "");
+  TextEditingController t2 = TextEditingController(text: "");
+
+
+  doAddition() {
+  setState(() {
+    num2 = double.parse(t2.text);
+    num1 = double.parse(t1.text);
+    output= num1 + num2;
+  });
+}
+
+doSubstraction() {
+  setState(() {
+    num2 = double.parse(t2.text);
+    num1 = double.parse(t1.text);
+    output= num1 - num2;
+  });
+}
+
+doMultiplication() {
+  setState(() {
+    num2 = double.parse(t2.text);
+    num1 = double.parse(t1.text);
+    output= num1 * num2;
+  });
+}
+
+doDivision() {
+  setState(() {
+    num2 = double.parse(t2.text);
+    num1 = double.parse(t1.text);
+    output= (num1 / num2);
+  });
+}
+
+doClear() {
+  setState(() {
+    t1 = TextEditingController(text: '');
+    t2 = TextEditingController(text: '');
+    output= 0;
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,106 +74,139 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('My Calc'),
       ),
       body: Container(
-        padding: EdgeInsets.only(bottom:40.0, top: 15.0, left: 40.0, right: 40.0),
+          padding:
+              EdgeInsets.only(bottom: 40.0, top: 15.0, left: 40.0, right: 40.0),
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: TextField(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: TextField(
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.purpleAccent,
+                    decoration: InputDecoration(
+                      labelText: 'output',
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(17.0),
+                      ),
+                      hintText: 'output: $output',
+                      hintStyle: TextStyle(
+                        color: Colors.purpleAccent,
+                        fontSize: 15.0,
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+              ),
+              TextField(
                 keyboardType: TextInputType.number,
+                controller: t1,
+
                 cursorColor: Colors.purpleAccent,
                 decoration: InputDecoration(
-                  labelText: 'output',
+                  labelText: 'first number',
                   fillColor: Colors.white,
+                  hintText: 'Enter your First number',
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(17.0),
+                    borderRadius: BorderRadius.circular(18.0),
                   ),
-                  hintText: 'Result: output',
-                  hintStyle: TextStyle(
-                    color: Colors.purpleAccent,
-                    fontSize: 15.0,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 15)),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: t2,
+                cursorColor: Colors.deepPurple,
+                decoration: InputDecoration(
+                  labelText: 'second number',
+                  fillColor: Colors.white,
+                  hintText: 'Enter your second number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(18.0),
                   ),
-                )),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 20.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            cursorColor: Colors.purpleAccent,
-            decoration: InputDecoration(
-              labelText: 'first number',
-              fillColor: Colors.white,
-              hintText: 'Enter your First number',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(18.0),
+                ),
               ),
-            ),
-          ),
-          Padding(padding: EdgeInsets.only(top: 15)),
-          TextField(
-            keyboardType: TextInputType.number,
-            cursorColor: Colors.deepPurple,
-            decoration: InputDecoration(
-              labelText: 'second number',
-              fillColor: Colors.white,
-              hintText: 'Enter your second number',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(18.0),
+              Padding(padding: EdgeInsets.only(top: 15)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MaterialButton(
+                    child: Text('+'),
+                    shape: StadiumBorder(),
+                    color: Colors.deepPurple,
+                    padding: EdgeInsets.only(
+                        left: 50.0, right: 50.0, top: 12.0, bottom: 12.0),
+                    onPressed: () {
+                    //TODO:
+                    doAddition();
+                  },
+                  ),
+                  MaterialButton(
+                    child: Text(
+                      '-',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    shape: StadiumBorder(),
+                    color: Colors.deepPurple,
+                    padding: EdgeInsets.only(
+                        left: 50.0, right: 50.0, top: 12.0, bottom: 12.0),
+                     onPressed: () {
+                    //TODO:
+                    doSubstraction();
+                  },
+                  ),
+                ],
               ),
-            ),
-          ),
-          Padding(padding: EdgeInsets.only(top: 15)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-               MaterialButton(
-                child: Text('+'),
-                shape: StadiumBorder(),
-                color: Colors.deepPurple,
-                onPressed: () {},
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
               ),
-                MaterialButton(
-                child: Text('-', style: TextStyle(fontWeight: FontWeight.bold),),
-                shape: StadiumBorder(),
-                color: Colors.deepPurple,
-                onPressed: () {},
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MaterialButton(
+                    child: Text('*'),
+                    shape: StadiumBorder(),
+                    color: Colors.deepPurple,
+                    padding: EdgeInsets.only(
+                        left: 50.0, right: 50.0, top: 12.0, bottom: 12.0),
+                  onPressed: () {
+                    //TODO:
+                    doMultiplication();
+                  },
+                  ),
+                  MaterialButton(
+                    child: Text('/'),
+                    shape: StadiumBorder(),
+                    color: Colors.deepPurple,
+                    padding: EdgeInsets.only(
+                        left: 50.0, right: 50.0, top: 12.0, bottom: 12.0),
+                    onPressed: () {
+                    //TODO:
+                    doDivision();
+                  },
+                  ),
+                ],
               ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MaterialButton(
+                    child: Text('clear'),
+                    shape: StadiumBorder(),
+                    padding: EdgeInsets.only(
+                        left: 120.0, right: 120.0, top: 12.0, bottom: 12.0),
+                    color: Colors.red,
+                    onPressed: () {},
+                  ),
+                ],
+              )
             ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-            
-              MaterialButton(
-                child: Text('*'),
-                shape: StadiumBorder(),
-                color: Colors.deepPurple,
-                onPressed: () {},
-              ),
-              MaterialButton(
-                child: Text('/'),
-                shape: StadiumBorder(),
-                color: Colors.deepPurple,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              MaterialButton(
-                child: Text('clear'),
-                shape: StadiumBorder(),
-                 padding: EdgeInsets.only(left: 120.0, right: 120.0, top: 12.0, bottom: 12.0),
-                color: Colors.deepPurple,
-                onPressed: () {},
-              ),
-            ],
-          )
-        ],
-      )),
+          )),
     );
   }
 }
